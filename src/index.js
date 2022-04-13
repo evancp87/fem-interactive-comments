d5bbcc3e4269e14bfd87de7c5248d49b05454284
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-04-13 16:21:57 +0100

    packages and data added- including middleware, reducers and createStore

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import {Provider } from 'react-redux';
+// middleware, reducer
+import reducer from './reducers';
+import middleware from './middleware';
+
+const store = createStore(reducer, middleware);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+
     <App />
-  </React.StrictMode>,
+  </Provider>
+,
   document.getElementById('root')
 );
 
