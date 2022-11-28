2ca477a393ef4a81f61af159afaf2a0add79425e
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-11-28 19:28:23 +0000

    tests added

--- src/__tests__/posts.test.js
+++ src/__tests__/posts.test.js
@@ -1,5 +1,87 @@
-describe(" function", () => {
-  test("it should ", () => {
-    // actual test
+import * as postState from "../features/categories/postsSlice";
+
+describe("Category reducer", () => {
+  test("reducers", () => {
+    let state;
+    state = reducers(
+      {
+        categories: {
+          categories: [
+            { name: "react", path: "react" },
+            { name: "redux", path: "redux" },
+            { name: "udacity", path: "udacity" },
+          ],
+        },
+        comments: { comments: [] },
+        posts: { posts: [] },
+      },
+      {
+        type: "posts/receivePosts/fulfilled",
+        payload: [
+          {
+            id: "8xf0y6ziyjabvozdd253nd",
+            timestamp: 1467166872634,
+            title: "Udacity is the best place to learn React",
+            body: "Everyone says so after all.",
+            author: "thingtwo",
+            category: "react",
+            voteScore: 6,
+            deleted: false,
+            commentCount: 2,
+          },
+          {
+            id: "6ni6ok3ym7mf1p33lnez",
+            timestamp: 1468479767190,
+            title: "Learn Redux in 10 minutes!",
+            body: "Just kidding. It takes more than 10 minutes to learn technology.",
+            author: "thingone",
+            category: "redux",
+            voteScore: -5,
+            deleted: false,
+            commentCount: 0,
+          },
+        ],
+        meta: {
+          requestId: "n4HnWj8-PeaJjbVfTToYM",
+          requestStatus: "fulfilled",
+        },
+      }
+    );
+    expect(state).toEqual({
+      categories: {
+        categories: [
+          { name: "react", path: "react" },
+          { name: "redux", path: "redux" },
+          { name: "udacity", path: "udacity" },
+        ],
+      },
+      comments: { comments: [] },
+      posts: [
+        [
+          {
+            id: "8xf0y6ziyjabvozdd253nd",
+            timestamp: 1467166872634,
+            title: "Udacity is the best place to learn React",
+            body: "Everyone says so after all.",
+            author: "thingtwo",
+            category: "react",
+            voteScore: 6,
+            deleted: false,
+            commentCount: 2,
+          },
+          {
+            id: "6ni6ok3ym7mf1p33lnez",
+            timestamp: 1468479767190,
+            title: "Learn Redux in 10 minutes!",
+            body: "Just kidding. It takes more than 10 minutes to learn technology.",
+            author: "thingone",
+            category: "redux",
+            voteScore: -5,
+            deleted: false,
+            commentCount: 0,
+          },
+        ],
+      ],
+    });
   });
 });
