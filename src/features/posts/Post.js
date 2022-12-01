9714f056ac5dde124c8098243c09d74e6a280914
Evan Parker <65721884+evancp87@users.noreply.github.com>
2022-12-01 23:47:19 +0000

    individual Post and postlist components built out

--- src/features/posts/Post.js
+++ src/features/posts/Post.js
@@ -1,34 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // import DeleteButton from "./DeleteButton";
 // import EditButton from "./EditButton";
 
 import Vote from "../../components/Vote";
-function Post(props) {
+function Post({ posts }) {
+  console.log({ posts });
   return (
-    <div className="max-w-6xl mb-4 shadow-xl w-[200px] hover:shadow-2xl sm:w-[300px] md:w-[500px] lg:w-[900px] h-auto rounded-md bg-slate-100 h-50 p-2.5 flex flex-col sm:flex-row md:flex-row lg:flex-row justify-between space-x-4  ">
-      <div className="flex flex-row">
-        <Vote voteScore={props.voteScore} />
+    <div>
+      {posts &&
+        posts.map((post, i) => (
+          <div
+            key={i}
+            className="max-w-6xl mb-4 shadow-xl w-[200px] hover:shadow-2xl sm:w-[300px] md:w-[500px] lg:w-[900px] h-auto rounded-md bg-slate-100 h-50 p-2.5 flex mx-auto flex-col sm:flex-row md:flex-row lg:flex-row justify-between space-x-4  "
+          >
+            <div className="flex flex-row">
+              <Vote voteScore={post.voteScore} />
 
-        <div className="ml-6 flex flex-col space-x-3 max-w-lg">
-          <div className="flex mb-4 flex-row flex-wrap ml-2 space-x-4 justify-baseline">
-            <img
-              className=" rounded-full  ml-4 sm:ml-0 flex-row w-5 h-5 "
-              src="https://images.pexels.com/photos/13366951/pexels-photo-13366951.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-            />
+              <div className="ml-6 flex flex-col space-x-3 max-w-lg">
+                <div className="flex mb-4 flex-row flex-wrap ml-2 space-x-4 justify-baseline">
+                  <img
+                    className=" rounded-full  ml-4 sm:ml-0 flex-row w-5 h-5 "
+                    src="https://images.pexels.com/photos/13366951/pexels-photo-13366951.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
+                  />
 
-            <p className="text-sm ">{props.author}</p>
-            <p className=" text-sm">{props.timestamp}</p>
-            <p className=" text-sm">{props.category}</p>
+                  <p className="text-sm ">
+                    {post.author ? post.author : "no author"}
+                  </p>
+                  <p className=" text-sm">{post.timestamp}</p>
+                  <p className=" text-sm">{post.category}</p>
+                </div>
+                {/* <Link
+            to={{ pathname: `/posts/${props.id}` }}
+            // to={`/posts/${props.id}`}
+          > */}
+                <Link to={`/posts/${post.id}`}>
+                  <h2 className=" text-lg">{post.title}</h2>
+                  <p className="text-base">{post.body}</p>
+                </Link>
+                {/* </Link> */}
+              </div>
+            </div>
+            <div className="space-x-3 mt-4 flex-row flex sm:mt-0  md:mt-0 lg:mt-0 sm:flex-col  md:flex-col lg:flex-col justify-between items-center ">
+              <p className="text-sm"> {post.commentCount}</p>
+            </div>
           </div>
-
-          <h2 className=" text-lg">{props.title}</h2>
-          <p className="text-base">{props.body}</p>
-        </div>
-      </div>
-      <div className="space-x-3 mt-4 flex-row flex sm:mt-0  md:mt-0 lg:mt-0 sm:flex-col  md:flex-col lg:flex-col justify-between items-center ">
-        <p className="text-sm"> {props.commentCount}</p>
-      </div>
+        ))}
     </div>
   );
 }
--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,19 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Link } from "react-router-dom";
 
-// import {
-//   receivePosts,
-//   addPost,
-//   updatePost,
-//   votePost,
-//   increaseCommentCount,
-//   deductCommentCount,
-//   sortPostshigh,
-//   sortPostsLow,
-//   sortPostsRecent,
-//   sortPostsOld,
-// } from "../actions/postsActions";
 import Post from "./Post";
 
 function PostList() {
@@ -22,25 +9,8 @@ function PostList() {
 
   return (
     <section className="mt-4 flex items-center flex-col">
-      {posts &&
-        posts.map((post, i) => (
-          <Link to={`/posts/${post.id}`}>
-            <Post
-              key={i}
-              timestamp={post.timestamp}
-              body={post.body}
-              author={post.author}
-              category={post.category}
-              voteScore={post.voteScore}
-              commentCount={post.commentCount}
-              title={post.title}
-            />
-          </Link>
-        ))}
-      {/* <div key={i}>{post.body}</div> */}
-      <Post />
+      <Post posts={posts} />
     </section>
-    //   Object.keys(defaultData).map(post => <Post/>)
   );
 }
 
